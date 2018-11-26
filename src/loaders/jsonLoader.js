f0d428aa5cb073c3f724ee2c253ad2f235d214f8
Евгений Чирковский <evgen@Mac-mini.local>
2018-11-26 23:18:15 +0300

    loader functionality and validation

--- src/loaders/jsonLoader.js
+++ src/loaders/jsonLoader.js
@@ -1,7 +1,31 @@
 const loaderUtils = require('loader-utils');
+const validateOptions = require('schema-utils');
+const schema = require('./options.json');
 
 module.exports = function(content, map, meta) {
-	const options = loaderUtils.getOptions(this);
-	console.log(content, options);
-	return content;
+	const options = loaderUtils.getOptions(this) || null;
+
+	validateOptions(schema, options, 'JSON loader');
+
+	let filePath = this.resourcePath;
+	let fileName = filePath.slice(filePath.lastIndexOf('/'));
+
+	let contentObj = JSON.parse(content);
+
+	if (options) {
+		for (key in contentObj) {
+			let val = contentObj[key];
+			if ((options.invertBool)&&(typeof val === 'boolean')) {
+				contentObj[key] = val ? false : true;
+			}
+			if ((options.toUppercase)&&(typeof val === 'string')) {
+				contentObj[key] = val.toUpperCase();
+			}
+		}
+	}
+
+	let result = JSON.stringify(contentObj);
+
+	this.emitFile(fileName, result);
+	return result;
 };
